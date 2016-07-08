46fd1cb54ee3093bab1fff711581820ae9d0b619
Zachariah Young <zpyoung@gmail.com>
2016-07-08 01:22:56 -0500

    update configurations

--- src/main/java/com/zandroid/DockerZuulApplication.java
+++ src/main/java/com/zandroid/DockerZuulApplication.java
@@ -2,10 +2,12 @@ package com.zandroid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableHystrixDashboard
 public class DockerZuulApplication {
 
 	public static void main(String[] args) {
